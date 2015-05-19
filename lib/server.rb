cc7041610224c2e8bf4caee72fc3c2aeb2a33528
andygout <andygout@hotmail.co.uk>
2015-05-19 17:02:51 +0100

    Completed: Stage 1

--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
-require './lib/link'
+require 'link'
+require 'tag'
 
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
@@ -9,7 +10,24 @@ DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello BookmarkManager!'
+    @links = Link.all
+    erb :index
+  end
+
+  post '/links' do
+    url = params['url']
+    title = params['title']
+    tags = params['tags'].split(' ').map do |tag|
+      Tag.first_or_create(text: tag)
+    end
+    Link.create(url: url, title: title, tags: tags)
+    redirect to('/')
+  end
+
+  get '/tags/:text' do
+    tag = Tag.first(text: params[:text])
+    @links = tag ? tag.links : []
+    erb :index
   end
 
   # start the server if ruby file executed directly
