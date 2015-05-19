d6d3a8ed8018b5c8c295e211460478b09cd7e01f
andygout <andygout@hotmail.co.uk>
2015-05-19 17:51:10 +0100

    Completed: Stage 2

--- lib/server.rb
+++ lib/server.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require 'data_mapper'
 require 'link'
 require 'tag'
+require 'user'
 
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
@@ -9,6 +10,10 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     @links = Link.all
     erb :index
@@ -28,6 +33,23 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(text: params[:text])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
   # start the server if ruby file executed directly
