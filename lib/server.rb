d510069d48a025e6cfc962fbe3aa105f540528bc
andygout <andygout@hotmail.co.uk>
2015-05-19 20:00:34 +0100

    Completed: Stage 8

--- lib/server.rb
+++ lib/server.rb
@@ -22,6 +22,8 @@ class BookmarkManager < Sinatra::Base
     flash.now[:notice] = "Thanks for signing up!"
   end
 
+   use Rack::MethodOverride
+
   get '/' do
     @links = Link.all
     erb :index
@@ -59,6 +61,28 @@ class BookmarkManager < Sinatra::Base
       flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
     end
+  end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password]
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash[:errors] = ['The email or password is incorrect']
+      erb :'sessions/new'
+    end
+  end
+
+  delete '/sessions' do
+    flash[:notice] = 'Good bye!'
+    session[:user_id] = nil
+    redirect to('/')
   end
 
   helpers do
