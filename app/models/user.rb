1d93b38d9f6af77c53140ef58ce4b4da3ac01369
andygout <andygout@hotmail.co.uk>
2015-05-21 18:40:12 +0100

    Mailgun working with token a href link

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,14 +36,13 @@ class User
     smtp_login = ENV['MAILGUN_SMTP_LOGIN']
     smtp_login_seg = ENV['MAILGUN_SMTP_LOGIN_SEG']
     email_address = ENV['AG_EMAIL_ADDRESS']
-    token_url = "localhost:9292/users/password_reset/#{self.password_token}"
-
+    token_url = "<a href='http://localhost:9292/users/password_reset/#{self.password_token}'>here</a>"
     token_deadline = (self.password_token_timestamp + (60 * 60)).strftime("%H:%M on %d %b %Y")
 
     RestClient.post "https://api:#{api_key}@api.mailgun.net/v3/#{smtp_login_seg}.mailgun.org/messages",
-    :from => "Bookmark Manager <#{smtp_login}>",
-    :to => "<#{email_address}>",
-    :subject => "Email recovery token",
-    :text => "You have requested an email recovery token; please follow the below link to reset your password. It must be used by #{token_deadline}. Go to #{token_url}."
+    from: "Bookmark Manager <#{smtp_login}>",
+    to: "<#{email_address}>",
+    subject: "Email recovery token",
+    html: "You have requested an email recovery token. Please click #{token_url} to reset your password.  It must be used by #{token_deadline}."
   end
 end
